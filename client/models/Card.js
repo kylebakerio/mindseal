ca3fcad23ca7737ea66b3fc8080e050c511eeedc
Nathan Leonard <n.leonard668@gmail.com>
2015-09-04 15:50:04 -0500

    Added card and deck models

---
+++ client/models/Card.js
@@ -0,0 +1,37 @@
+Card = Card || {};
+
+Card.vm = function (props) {
+  props = props || {};
+
+  return {  
+    front: m.prop(props.front || ''),
+    back:  m.prop(props.back || ''),
+    id:    m.prop(props.id || 0),
+    flag:  m.prop(props.flag || '')
+  }
+}
+
+
+// these functions need to be redefined, but we should save these
+//implementations for reference.
+
+  // remove = function(index) {
+  //   var toRemove = ctrl.contacts().splice(index, 1);
+  //   m.request({
+  //     method: 'DELETE',
+  //     url: '/decks/' + options.deck,
+  //     data: toRemove
+  //   });
+  // }
+
+  // update = function(index) {
+  //   var toUpdate = ctrl.contacts()[index];
+  //   m.request({
+  //     method: 'POST',
+  //     url: '/decks/' + options.deck,
+  //     data: toUpdate
+  //   })
+  //   .then(function(updatedCard) {
+  //     ctrl.cards()[index] = updatedCard;
+  //   })
+  // }
