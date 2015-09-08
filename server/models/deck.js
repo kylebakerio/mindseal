ccdd0c164b33b3d05b45747634b07e723d471da0
mishasaggi <monisha.saggi@gmail.com>
2015-09-08 01:22:16 -0500

    getCards method working as well

--- server/models/deck.js
+++ server/models/deck.js
@@ -8,32 +8,45 @@ var db = pmongo(url, ['decks']);
 var collection = db.collection('decks');
 
 var decksMethods = module.exports; // decksMethods change name
+var testString = "55ee4e4fba73cb4b51a3a264"; //for hardcoded deck id
 
 decksMethods.find = function (id) {
-  return collection.findOne({ _id: pmongo.ObjectId(id) })
-          .then(function(doc) { console.log(doc, "i"); });
+  return collection.findOne({ _id: pmongo.ObjectId(id) });
+          // .then(function(doc) { console.log("in find")}); //since the .then part is happening in calling functions this needs to be removed. tested.
           }
 
 decksMethods.create = function (name, user) { //creates a new empty deck. MVP: pre-create one.
   user = user || 'MvpTester'; //hardcoded user for MVP
-  collection.insert({ name: name, cards: {} });
+  collection.insert({ name: name, cards: {} }); 
 }
-
+//are we adding/editing cards client side instead and pushing whole deck objects here?
 decksMethods.addCards = function (id, card) { //add cards (one at a time) to an existing deck.
-  id = id || "55ee4e4fba73cb4b51a3a264" //MVP: single hard coded value, only send the id string
-  decksMethods.find(id)
-  .then(function(deck){
-    // deck.update({ $push: { card: card } });
-    deck.update( { $push: { cards: {order: "card3" } }}, { cards: { card3: card } } );
+  var id = id || testString; //MVP: single hard coded value, only send the id string
+  // decksMethods.find(id)
+  // .then(function(deck){
+  //   // deck.update({ $push: { card: card } });
+  //   // fix to add to deck a card object
+  //   // deck.update( { $push: { cards: { order: "card3" } }}, { cards: { card3: card } } );
+  // })
+
+  //another approach
+  collection.findAndModify({
+    query: { _id: id },
+    update: { $set: { cards: { card3: card } } },
+    new: true
   })
+  .then(function(deck) {
+    console.log(deck, "here");
+  });
 }
 
 decksMethods.getCards = function (user) { //calls user.js to get all deck ids for that user MVP: default user
   user = user || 'MvpTester'; //arg undefined for MVP
-  allDecks = "55edd797ba73cb4b51a3a262" //should be an array of deck ids. single hardcoded value for MVP, send only the id string
+  var allDecks = testString; //should be an array of deck ids. single hardcoded value for MVP, send only the id string
   decksMethods.find(allDecks)
   .then(function(deck){ //decks objects
-    return deck; //array of deck
+    console.log(deck);
+    return deck;
   })
 }
 
