fc902a145b9ecb30f32dde9c625adaad1346a8bf
mishasaggi <monisha.saggi@gmail.com>
2015-09-11 18:29:45 -0500

    added ternary statments to handle testing with no user

--- server/models/userData.js
+++ server/models/userData.js
@@ -14,8 +14,8 @@ var collection = db.collection('decks');
 //making database model available to the server
 var decksMethods = module.exports; 
 
-decksMethods.user = function (userId, userName) {
-  return collection.insert({_id: name}, name: userName, decks: {});
+decksMethods.createUser = function (userId, userName) {
+  return collection.insert({ _id: name, name: userName, decks: {} });
 }
 
 decksMethods.createDecks = function (userId, deckName, deck) { //create a deck for a specific user
@@ -24,11 +24,13 @@ decksMethods.createDecks = function (userId, deckName, deck) { //create a deck f
   setObject["$set"] = {}; //creating a variable for the set part of the update query
   setObject["$set"]["decks."+deckName] = deck; //creating a variable key to take in the name of the deck
 
-  return collection.update({_id: userId}, setObject);
+  return userId ? collection.update({_id: userId}, setObject) : collection.update({_id: "uniqueUserGoogleId"}, setObject)
+  // return collection.update({_id: userId}, setObject);
 }
 
 decksMethods.getDecks = function (userId) { //get all decks of a user
-  return collection.findOne({_id: userId}); //return decks with deckname 
+  return userId ? collection.findOne({_id: userId}) : collection.find() //test code
+  // return collection.findOne({_id: userId}); //return decks with deckname 
 }
 
 decksMethods.refreshDecks = function (userId, decks) { //replaces the current decks in database completely
