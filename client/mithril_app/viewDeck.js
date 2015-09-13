377a8897924dc1869595172e7e81f0d6b0030401
finetype <finesttype@gmail.com>
2015-09-13 16:31:15 -0500

    nearly complete functionality, cleaner code. two bugs still being worked on... some things don't redraw when I expect them too. Also, moment.js related functionality in viewDeck is broken.

--- client/mithril_app/viewDeck.js
+++ client/mithril_app/viewDeck.js
@@ -1,25 +1,5 @@
 var viewDeck = {};
 
-viewDeck.rate = function(button){
-  var convert = {
-    'Did not remember': viewDeck.currentCard().cScale[0],
-    'Hard': viewDeck.currentCard().cScale[1],
-    'Good': viewDeck.currentCard().cScale[2],
-    'Too Easy': viewDeck.currentCard().cScale[3]
-  }
-
-  viewDeck.currentCard().tVal *= convert[button];
-  viewDeck.currentCard().timeLastSeen = moment(); //it was just seen now.
-  viewDeck.currentCard().toBeSeen = ( 
-    viewDeck.currentCard().timeLastSeen.clone().add(viewDeck.currentCard().tVal, 'milliseconds') 
-    );
-  console.log("Days to next viewing: " + 
-    moment.duration(viewDeck.currentCard().toBeSeen.diff(viewDeck.currentCard().timeLastSeen), 'milliseconds').asDays()
-    );
-  viewDeck.nextCard();
-  viewDeck.showBack();
-}
-
 viewDeck.view = function(){
   // document.getElementById("see-decks").addClass("active")
   //^potentially different way of handling highlighting parts of the nav bar?
@@ -30,34 +10,18 @@ viewDeck.view = function(){
       m("input[type='button']",{value:("Back to " + viewDeck.name + "'s dashboard")})
     ),
     m(".starter-template", [
-      m('p', "you are studying:"),
       m("h1", viewDeck.name),
+      m('br'),
       m('strong','cards remaining in deck:'),m('br'),
       // m('strong','minutes studied today:'),m('br'), //would be nice.
       m('br'),
       m(".center-block", [
-        m(".card.front.center-block", viewDeck.currentCard().front),
+        viewDeck.front(),
         m('br'),
-        m("",viewDeck.back())
+        viewDeck.back()
       ])
     ])
   ])
-}
-
-viewDeck.nextCard = function () {
-  if(viewDeck.currentDeck().cards.length <= viewDeck.index +1/* ||
-     moment().diff(viewDeck.currentDeck()[viewDeck.index +1].toBeSeen)*/
-    ) {
-    console.log("no more cards!")
-    //should set the containing div to be equal to a 'no more cards to view' message.
-    //with an overtime button.
-  }
-
-  //
-  else {
-    viewDeck.index++;
-    viewDeck.currentCard(viewDeck.currentDeck().cards[viewDeck.index]);
-  }
 }
 
 viewDeck.controller = function(args){
@@ -67,43 +31,78 @@ viewDeck.controller = function(args){
   viewDeck.index = 0;
   viewDeck.name = args.name;
   viewDeck.currentCard = m.prop(viewDeck.currentDeck().cards[viewDeck.index]);
+
+  viewDeck.rate = function(button){
+    var convert = {
+      'Did not remember': viewDeck.currentCard().cScale[0],
+      'Hard': viewDeck.currentCard().cScale[1],
+      'Good': viewDeck.currentCard().cScale[2],
+      'Too Easy': viewDeck.currentCard().cScale[3]
+    }
+
+    viewDeck.currentCard().tVal *= convert[button];
+    viewDeck.currentCard().timeLastSeen = moment(); //it was just seen now.
+    viewDeck.currentCard().toBeSeen = ( 
+      viewDeck.currentCard().timeLastSeen.clone().add(viewDeck.currentCard().tVal, 'milliseconds') 
+    );
+    console.log("Days to next viewing: " + 
+      moment.duration(viewDeck.currentCard().toBeSeen.diff(viewDeck.currentCard().timeLastSeen), 'milliseconds').asDays()
+    );
+    viewDeck.nextCard();
+    viewDeck.showBack();
+  }
+
+  viewDeck.noMore = function(){
+    viewDeck.front([m("h1.center-block", "Great work!"),m('p.lead','No more cards to view for now.')])
+    viewDeck.back(m('br')) //should be an overtime button
+    m.redraw()
+    console.log("noMore ran")
+  }
+
+  viewDeck.nextCard = function () {
+    if(viewDeck.currentDeck().cards.length <= viewDeck.index +1/* ||
+       moment().diff(viewDeck.currentDeck()[viewDeck.index +1].toBeSeen)*/
+      ) {
+      viewDeck.noMore()
+    }
+    else {
+      viewDeck.index++;
+      viewDeck.currentCard(viewDeck.currentDeck().cards[viewDeck.index]);
+    }
+  }
+
   viewDeck.showBack = function(){
-    if (viewDeck.show !== 1){
+    if (viewDeck.show !== true){
       viewDeck.back([
         m(".card.back.center-block", viewDeck.currentCard().back),
         m('br'),
-        m("input",{type:'button', onclick: m.withAttr("value", viewDeck.rate), value:'Did not remember'}),
-        m("input",{type:'button', onclick: m.withAttr("value", viewDeck.rate), value:'Hard'}),
-        m("input",{type:'button', onclick: m.withAttr("value", viewDeck.rate), value:'Good'}),
-        m("input",{type:'button', onclick: m.withAttr("value", viewDeck.rate), value:'Too Easy'})
+        m("input",{type:'button', onclick: m.withAttr("value", viewDeck.rate), value:'Did not remember', title:"Press 0 to select"}),
+        m("input",{type:'button', onclick: m.withAttr("value", viewDeck.rate), value:'Hard', title:"Press 1 to select"}),
+        m("input",{type:'button', onclick: m.withAttr("value", viewDeck.rate), value:'Good', title:"Press 2 to select"}),
+        m("input",{type:'button', onclick: m.withAttr("value", viewDeck.rate), value:'Too Easy', title:"Press 3 to select"})
       ])
-      viewDeck.show = 1;
+      viewDeck.show = true;
     }
     else {
-      viewDeck.show = 0;
+      viewDeck.show = false;
       viewDeck.back([
         m('br'),
-        m("input",{type:'button', onclick: viewDeck.showBack, value:'Show Back'})
+        m("input",{type:'button', onclick: viewDeck.showBack, value:'Show Back', title:'Press spacebar to select'})
       ])
+      viewDeck.front = m.prop(
+        m(".card.front.center-block", viewDeck.currentCard().front)
+      )
     }
   }
 
   viewDeck.back = m.prop([
     m('br'),
-    m("input",{type:'button', onclick:viewDeck.showBack, value:'Show Back'})
+    m("input",{type:'button', onclick: viewDeck.showBack, value:'Show Back', title:'Press spacebar to select'})
   ])
-  //should be called on every button press
-  // ctrl.rate = function (flag) {
-  //   var toRate /*= ctrl.contacts().splice(, 1);*/ //should be the card...
-  //   m.request({
-  //     method: 'POST',
-  //     url: '/decks/' + options.deck,
-  //     data: toRate
-  //   });
-  // //   var newModel = new Contacts.model()
-  // //   ctrl.contacts().push(newModel)
-  //   if (!ctrl.deck[cardIndex + 1].flag) //if the flag of the next card indicates it should be seen...
-  //     ctrl.nextCard() //then run the next card function
-  // }
+
+
+  viewDeck.front = m.prop(
+    m(".card.front.center-block", viewDeck.currentCard().front)
+  )
 
 }
