c41336cc83b2ba0f380993ab91f5f1714c726c03
finetype <finesttype@gmail.com>
2015-11-03 17:22:47 -0600

    added 'alpha release' to landing page, removed popup

--- client/mithril_app_new/landing.js
+++ client/mithril_app_new/landing.js
@@ -2,47 +2,54 @@
   window.Landing = {};
 
   Landing.view = function(ctrl){  
-    return m(".cow.valign-wrapper", [
-      m(".row", [
-        m(".hoverable.card-panel.col.m5.offset-m1.l4.offset-l2.s12.top-bottom-pad-mar.valign-wrapper", [
-          m("form.col.s12", [
-            m(".container.card-content", [
-              m('p','Sign Up or Login using this form.'),
-              m(".row", [
-                m(".input-field.col.s12", [
-                  m("input.validate[id='username'][type='text']", {onchange: m.withAttr("value", ctrl.username)}),
-                  m("label[for='username']", "Username")
-                ])
-              ]),
-              m(".row", [
-                m(".input-field.col.s12", [
-                  m("input.validate[id='password'][type='password']", {onchange: m.withAttr("value", ctrl.password)}),
-                  m("label[for='password']", "Password")
-                ])
-              ]),
-              m(".row", [
-                m(".input-field.col.s12", [
-                  m("input.validate[id='email'][type='email']", {onchange: m.withAttr("value", ctrl.email)}),
-                  m("label[for='email']", "Email (if signing up)")
-                ])
-              ]),
-              m("a.col.s4.waves-effect.waves-light.btn-large", {onclick:ctrl.signUp}, [m("i.material-icons.right", "person_add"),"Sign Up"]),
-              m("a.col.s4.waves-effect.waves-light.btn-large", {onclick:ctrl.login}, [m("i.material-icons.right", "person"),"Login"])
+    return m(".cow",[
+    m(".row", [
+      m(".col.m5.offset-m3.l4.offset-l4.s12",[
+        m("p", "Alpha release, under active development. ")
+        ])
+      ]),
+      m(".cow.valign-wrapper", [
+        m(".row", [
+          m(".hoverable.card-panel.col.m5.offset-m1.l4.offset-l2.s12.top-bottom-pad-mar.valign-wrapper", [
+            m("form.col.s12", [
+              m(".container.card-content", [
+                m('p','Sign Up or Login using this form.'),
+                m(".row", [
+                  m(".input-field.col.s12", [
+                    m("input.validate[id='username'][type='text']", {onchange: m.withAttr("value", ctrl.username)}),
+                    m("label[for='username']", "Username")
+                  ])
+                ]),
+                m(".row", [
+                  m(".input-field.col.s12", [
+                    m("input.validate[id='password'][type='password']", {onchange: m.withAttr("value", ctrl.password)}),
+                    m("label[for='password']", "Password")
+                  ])
+                ]),
+                m(".row", [
+                  m(".input-field.col.s12", [
+                    m("input.validate[id='email'][type='email']", {onchange: m.withAttr("value", ctrl.email)}),
+                    m("label[for='email']", "Email (if signing up)")
+                  ])
+                ]),
+                m("a.col.s4.waves-effect.waves-light.btn-large", {onclick:ctrl.signUp}, [m("i.material-icons.right", "person_add"),"Sign Up"]),
+                m("a.col.s4.waves-effect.waves-light.btn-large", {onclick:ctrl.login}, [m("i.material-icons.right", "person"),"Login"])
+              ])
             ])
-          ])
-        ]),
-        window.innerWidth < 600 ? 
-        null
-        :
-        m(".card-panel.col.s12.m5.l4.top-bottom-pad-mar.valign-wrapper", [
-          m(".col.s12", [
-            m(".container.card-content", [
-              m("h5", [" What is ",m("i", "mind:seal"),"? "]),
-              m("p.home-text", "mind:seal is a spaced repitition flashcard app designed to be as simple, fast, and clean as possible. It uses a machine learning algorithm to determine when you should see facts, and adapts to your personal usage patterns over time, for each individual card. Join to see shared decks, or create your own!")
+          ]),
+          window.innerWidth < 600 ? 
+          null
+          :
+          m(".card-panel.col.s12.m5.l4.top-bottom-pad-mar.valign-wrapper", [
+            m(".col.s12", [
+              m(".container.card-content", [
+                m("h5", [" What is ",m("i", "mind:seal"),"? "]),
+                m("p.home-text", "mind:seal is a spaced repitition flashcard app designed to be as simple, fast, and clean as possible. It uses a machine learning algorithm to determine when you should see facts, and adapts to your personal usage patterns over time, for each individual card. Join to see shared decks, or create your own!")
+              ])
             ])
           ])
         ])
-      ])
+      ])//
     ])
   };
 
@@ -73,13 +80,9 @@
       //User.forgot(ctrl.email())
     }
 
-    if (/*logged in*/typeof App === 'object' && typeof App.mindSeal === 'object') {
+    if (/*logged in*/typeof App === 'object' && typeof App.mindSeal === 'object') 
       console.log("App.mindSeal is an object")
       m.route("/home");
-    }
-    else {
-      alert("Please excuse our bugs and lack of polish! This is an alpha release. Things should be working, but can break occassionally due to active development. :)")
-    }
 
   };
 })()
