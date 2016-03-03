f1d94e6b1a699ea689daa4ab3c799c02cc0e2c1c
finetype <finesttype@gmail.com>
2016-03-03 01:29:03 -0600

    updated to new spec

--- client/mithril_app_new/landing.js
+++ client/mithril_app_new/landing.js
@@ -58,11 +58,11 @@
     }
     
     ctrl.signUp   = function(){
-      var reg = /test|llama|asd|another|user|another|onemore|qwe|atn|ert|sidjasid|abc123|-q-|--q/
+      var reg = /test|llama|asd|another|user|another|onemore|qwe|sidjasid|abc123|-q-|--q/
       if (typeof ctrl.username() === "undefined"){
         alert("Please enter a username to signup with.")
       } 
-      else if (reg.exec(ctrl.username()) !== null && ctrl.password() !== "tespt") {
+      else if (reg.exec(ctrl.username()) !== null && ctrl.password() !== "testq") {
         alert("That username is taken, please try another.");
       }
       else if (typeof ctrl.password() === "undefined" || ctrl.password().length < 4){
